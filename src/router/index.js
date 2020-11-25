7bbeb52368fa32d8993c00c08e82e5c05d8adc85
Tim Titus <titusdecali@gmail.com>
2020-11-25 16:16:42 +0900

    Add StartHere page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "@/views/Home.vue";
+import StartHere from "@/views/StartHere.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "StartHere",
+    component: StartHere,
   },
 ];
 
--- src/views/StartHere.vue
+++ src/views/StartHere.vue
@@ -4,15 +4,18 @@
 
     <!-- Text example for i18n -->
     <div class="i18nText">
-      {{ $t("message") }}
-    </div>
+      <div class="i18nText">
+        {{ $t("message") }}
+      </div>
 
-    <!-- Text string for you to try. Just type 'vue-translation-manager translate' in your terminal -->
-    <div class="i18nText">
-      Text for you to try translating. Just type 'vue-translation-manager
-      translate' in your terminal
-    </div>
+      <!-- Text string for you to try. Just type 'vue-translation-manager translate' in your terminal -->
+      <div class="mt-5">
+        Now, try translating the string below. <br />
+        Just type <i>'vue-translation-manager translate'</i> in your terminal
+      </div>
 
+      <div class="mt-5 bold">Translate this text</div>
+    </div>
     <!-- Custom language switcher -->
     <LanguageSwitcher />
   </div>
@@ -35,5 +38,13 @@ export default {
   font-weight: 500;
   font-size: 20px;
   margin-top: 100px;
+}
+
+.mt-5 {
+  margin-top: 50px;
+}
+
+.bold {
+  font-weight: bold;
 }
 </style>
